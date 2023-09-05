c2fbadef5605092ec8b92bd9ce644b1d9e4c230e
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-09-05 23:39:45 +0530

    The main home page.

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layouts/Layout'
+
+const HomePage = () => {
+  return (
+    <Layout className="homepage layout" title={"SphereNet"}>
+      <div className="container">
+        
+      </div>
+    </Layout>
+  )
+}
+
+export default HomePage
