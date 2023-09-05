48493d61adee19cdcc34dbbcea401d17cc2df5e5
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-09-05 23:21:22 +0530

    The Navbar -> with conditional rendering

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import './Header.css'
+import { useAuth } from '../../context/auth'
+import { toast } from 'react-hot-toast';
+import { NavLink } from 'react-router-dom';
+
+
+const Header = () => {
+
+  const [ auth , setAuth ] = useAuth();
+
+  const handleLogout = () => {
+    setAuth({
+      ...auth,
+      user : null,
+      token : "",
+    })
+    localStorage.removeItem("auth")
+    toast.success("Logout Successfully")
+  }
+
+  return (
+    <div className='header larger-shadow'>
+      <div className="header-container">
+        <div className="logo">
+          <a href="/">
+            <h5>SphereNet</h5>
+          </a>
+        </div>
+        <div className="log">
+        {
+          !auth?.user? (
+            <div className="log">
+              <div>
+                <NavLink to="/login">Login</NavLink>
+              </div>
+              <div>
+                <NavLink to="/register">Sign Up</NavLink>
+              </div>
+            </div>
+          ) : (
+            <div className="log">
+              <div className="user-name">
+                {auth?.user?.firstName} {auth?.user?.lastName}
+              </div>
+              <NavLink 
+                onClick={handleLogout}
+                to="/login"
+                className="logout-btn"
+              >
+                LogOut
+              </NavLink>
+            </div>
+          )
+        }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
