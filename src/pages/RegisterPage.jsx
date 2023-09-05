60c20a360d5457389631aaf17db366b708ce5146
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-09-05 23:40:30 +0530

    The User Register Page.

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/Layouts/Layout'
+
+const Register = () => {
+  return (
+    <Layout title={"Register User"}>
+    </Layout>
+  )
+}
+
+export default Register
