1dd9877c663c011cabf96812f3f8461f1d264093
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-09-21 22:17:59 +0530

    feat(components): Implement Selectable Tags component
    - Created a new React component named Tag for handling selectable tags.
    - Fetched tag options from the server using Axios.
    - Added functionality to select and deselect tags.
    - Displayed selected tags with the ability to remove them.
    - Implemented keyboard navigation and highlighting for options.
    - Utilized CSS modules for styling.
    
    This commit introduces the Tag component, providing a way to select and display tags interactively.

---
+++ src/components/Tag/Tag.jsx
@@ -0,0 +1,99 @@
+import { useEffect, useRef, useState } from "react";
+import styles from "./select.module.css";
+import axios from "axios";
+
+const Tag = ({ value, onChange }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [highlightedIndex, setHighlightedIndex] = useState(0);
+  const [options, setOptions] = useState([]); // Store fetched options
+  const containerRef = useRef(null);
+
+  const fetchOptions = async () => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API}/v1/api/tags`);
+      setOptions(response.data.tags);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchOptions();
+  }, []);
+
+  const clearOptions = () => {
+    onChange([]);
+  };
+
+  const selectOption = (option) => {
+    if (value.includes(option)) {
+      onChange(value.filter((o) => o !== option));
+    } else {
+      onChange([...value, option]);
+    }
+  };
+
+  const isOptionSelected = (option) => {
+    return value.includes(option);
+  };
+
+  useEffect(() => {
+    if (isOpen) setHighlightedIndex(0);
+  }, [isOpen]);
+
+  return (
+    <div
+      ref={containerRef}
+      onBlur={() => setIsOpen(false)}
+      onClick={() => setIsOpen((prev) => !prev)}
+      tabIndex={0}
+      className={styles.container}
+    >
+      <span className={styles.value}>
+        {value.map((v) => (
+          <button
+            key={v._id}
+            onClick={(e) => {
+              e.stopPropagation();
+              selectOption(v);
+            }}
+            className={styles["option-badge"]}
+          >
+            {v.name}
+            <span className={styles["remove-btn"]}>&times;</span>
+          </button>
+        ))}
+      </span>
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          clearOptions();
+        }}
+        className={styles["clear-btn"]}
+      >
+        &times;
+      </button>
+      <div className={styles.divider}></div>
+      <div className={styles.caret}></div>
+      <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
+        {options.map((option, index) => (
+          <li
+            onClick={(e) => {
+              e.stopPropagation();
+              selectOption(option);
+              setIsOpen(false);
+            }}
+            onMouseEnter={() => setHighlightedIndex(index)}
+            key={option._id}
+            className={`${styles.option} ${isOptionSelected(option) ? styles.selected : ""
+              } ${index === highlightedIndex ? styles.highlighted : ""}`}
+          >
+            {option.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Tag;
