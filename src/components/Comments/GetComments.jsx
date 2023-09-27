0616dd055b6cd158d3ffdfd594887ed2ce9c1f32
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-09-27 17:44:36 +0530

    Implement GetComments component
    - Created a new React component named GetComments.
    - Fetched comments for a content (e.g., post) from the server using Axios.
    - Reversed the order of comments to display the most recent ones first.
    - Conditionally rendered a message when no comments are available.
    - Rendered individual comments using the ShowComments component.
    - Added appropriate error handling for the API request.
    
    This commit introduces the GetComments component, which fetches and displays comments for a specified content.

---
+++ src/components/Comments/GetComments.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import ShowComments from './ShowComments';
+
+const GetComments = ({ contentId , contentType }) => {
+
+  const [ comments , setComments ] = useState([]);
+
+  const getCommentOnContent = async(contentId, contentType) => {
+    try {
+      if(contentType === 'post'){
+        const response = await axios.get(`${process.env.REACT_APP_API}/v1/api/posts/comments/${contentId}`);
+        setComments(response?.data?.comments);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const reversedComments = [...comments].reverse();
+  
+  useEffect(() => {
+    if(contentId){
+      getCommentOnContent(contentId, contentType);
+    }
+  },[contentId , contentType])
+
+  return (
+    <div>
+      <div className='p-1 bg-g2 bc br mt-1'>
+        {
+          comments.length === 0 && (
+            <div className='ta-cen c-w'>
+              <h6>No Comments</h6>
+            </div>
+          )
+        }
+        {
+          reversedComments.map((commentId) => (
+            <div key={commentId}>
+              <ShowComments commentId={commentId} />
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default GetComments
