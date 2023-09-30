61e4f08de1650fcd384f8665b294ea335e389fba
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-09-30 09:53:38 +0530

    refactor: Update Login component
    The Login component has been updated to improve readability, styling, and role-based routing. Changes include:
    
    - Improved styling and layout for a better user experience.
    - Utilized a custom Button component for the login button.
    - Replaced hard-coded role-based navigation with dynamic routing based on user roles ('regular', 'verified', 'moderator', 'admin').
    - Enhanced code structure and readability.
    
    These changes result in a more user-friendly and maintainable login page.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,6 +5,7 @@ import { useNavigate , useLocation } from 'react-router-dom'
 import { toast } from 'react-hot-toast'
 import { useAuth } from '../context/auth'
 import '../stylesheets/loginPage.css'
+import Button  from '../components/Design Elements/Button/Button'
 
 const Login = () => {
 
@@ -35,8 +36,10 @@ const Login = () => {
 
         localStorage.setItem("auth" , JSON.stringify(response.data))
 
-        if( userRole === "regular" ) navigate(location.state || "/feed/user-feed")
-        if( userRole === "admin" ) navigate(location.state || "/feed/admin-feed")
+        if( userRole === "regular" ) navigate(location.state || "/regular-user/feed/home/post")
+        if( userRole === "verified" ) navigate(location.state || "/verified-user/feed/home/post")
+        if( userRole === "moderator" ) navigate(location.state || "/moderator-user/feed/home/post")
+        if( userRole === "admin" ) navigate(location.state || "/admin/feed/home/post")
 
         toast.success( response.data && response.data.message )      
       } else {
@@ -51,13 +54,13 @@ const Login = () => {
   
   return (
     <Layout title={"Login User"}>
-      <div className="login">
+      <div className="login bg-g1 c-w">
         <div className="container log-container">
-          <div className="main-log-container">
-            <h3>Login</h3>
+          <div className="main-log-container p-2 br bc bg-g2">
+            <h3 className="ta-cen pb-2">Login</h3>
             <form onSubmit={handleLoginSubmit}>
 
-              <div className="login-email">
+              <div className="login-email p-1">
                 <input 
                   type="email" 
                   value={email}
@@ -69,7 +72,7 @@ const Login = () => {
                 />
               </div>
 
-              <div className="login-password">
+              <div className="login-password p-1">
                 <input 
                   type="password" 
                   value={password}
@@ -81,10 +84,10 @@ const Login = () => {
                 />
               </div>
 
-              <div className="login-btn">
-                <button>
+              <div className="login-btn pr-1 register-form-button">
+                <Button appearance="normal">
                   Login
-                </button>
+                </Button>
               </div>
 
             </form>
